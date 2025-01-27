45303c86009307667bff23bd4ba4ee86e9934642
Songhyun080302 <songhyun080302@naver.com>
2025-01-27 18:46:59 +0900

    Update BenjaminQi.py

--- BenjaminQi.py
+++ BenjaminQi.py
@@ -1,18 +1,20 @@
+from collections import defaultdict
+
 def solve():
     N = int(input())
-    arr = list(map(int, input().split()))  
-    
-    from collections import defaultdic
-    count_map = defaultdict(set) 
-    
-  
-    for i in range(N - 1, 0, -1):
-        if arr[i] == arr[i - 1]: 
-            count_map[arr[i]].add(arr[i - 1])  
-    
-    distinct_moos = set()  
+    arr = list(map(int, input().split()))
+
+    count_map = defaultdict(set)
+    for i in range(N - 1):
+        if arr[i] != arr[i+1]: 
+            count_map[arr[i+1]].add(arr[i])  # Add the preceding element to the set
+
+    distinct_moos = set()
     for y in count_map:
         for x in count_map[y]:
-            distinct_moos.add((x, y)) 
-    
-    print(len(distinct_moos))  
+            distinct_moos.add((x, y, y))
+
+    print(len(distinct_moos))
+
+if __name__ == "__main__":
+    solve()
